7c51614d63cf99b0d194f3840361cfac6494255a
unknown <ranasingh061177@gmail.com>
2023-03-07 00:11:06 +0530

    first-draft

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,29 @@
-const icon= document.querySelector('.searchbutton')
-const search = document.querySelector('.searchbar')
+window.addEventListener("scroll", function () {
+  let navbar = document.getElementById("top");
+  if (window.pageYOffset >= 202) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
 
-searchbutton.onclick=function(){
-    search.classList.toggle('active')
-}
+document.querySelectorAll(".video-container video").forEach((vid) => {
+  vid.onclick = () => {
+    document.querySelector(".popup-video").style.display = "block";
+    document.querySelector("popup-video iframe").src = vid.getAttribute("src");
+  };
+});
+
+document.querySelector(".popup-video span").onclick = () => {
+  document.querySelector(".popup-video").style.display = "none";
+};
+
+window.addEventListener("load", function () {
+  setTimeout(function open(event) {
+    document.querySelector(".popup").style.display = "block";
+  }, 1000);
+});
+
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
